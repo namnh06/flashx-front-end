482c30da215a07f873fa8c90d9d68619570207b2
Nam H Nguyen <namnh.developer@gmail.com>
2024-03-20 17:11:58 -0600

    Chapter 16 - Finished it

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,5 +1,9 @@
-export default function Page () {
-    return (
-        <h1>Customers</h1>
-    );
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
+export default function Page() {
+  return <h1>Customers</h1>;
 }
