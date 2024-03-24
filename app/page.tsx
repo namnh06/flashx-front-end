dbeae9444659051b19262f2df3d5eca4bd9cbcab
Nam Hai Nguyen <namnh.developer@gmail.com>
2024-03-23 23:44:12 -0600

    Changed file name to yxe

--- app/page.tsx
+++ app/page.tsx
@@ -57,14 +57,14 @@ export default function Page() {
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/6 md:px-28 md:py-12">
           <Image
-            src="/flashx-desktop.png"
+            src="/flashyxe-desktop.png"
             width={1000}
             height={760}
             className="hidden md:block"
             alt="Screenshots of the dashboard project showing desktop version"
           />
           <Image
-            src="/flashx-mobile.png"
+            src="/flashyxe-mobile.png"
             width={560}
             height={620}
             className="block md:hidden"
